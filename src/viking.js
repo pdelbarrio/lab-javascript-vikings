f1ea718351ab7f523e9f9c19f29e13aa20ae1a03
Pablo del Barrio <pablodbb@gmail.com>
2021-03-28 18:00:42 +0200

    Superbonus added :sparkles:

--- src/viking.js
+++ src/viking.js
@@ -43,6 +43,7 @@ class Saxon extends Soldier {
   }
 }
 
+//BONUS- Iteration 4: War
 // War
 class War {
   constructor() {
@@ -76,5 +77,17 @@ class War {
       this.vikingArmy.splice(vikingIndex, 1);
     }
     return message;
+  }
+  //SUPER BONUS
+  showStatus() {
+    if (this.vikingArmy.length >= 1 && this.saxonArmy.length >= 1) {
+      return `Vikings and Saxons are still in the thick of battle.`;
+    }
+    if (this.vikingArmy.length === 0) {
+      return `Saxons have fought for their lives and survived another day...`;
+    }
+    if (this.saxonArmy.length === 0) {
+      return `Vikings have won the war of the century!`;
+    }
   }
 }
